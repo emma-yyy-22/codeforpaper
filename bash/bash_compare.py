7d8086b2fa245726aa4797651117bbdf288a7c96
Chenyang Wang <chenyang.wang@eng.ox.ac.uk>
2024-08-09 15:11:16 +0100

    Add a bash file

---
+++ bash/bash_compare.py
@@ -0,0 +1,13 @@
+import os
+
+drug_list = ['RIF', 'INH', 'EMB', 'PZA', 'SM', 'OFX', 'CAP', 'AK', 'KAN', 'MOX', 'CIP']
+model_list = ['WDNN', 'DeepAMR', 'CNNGWP']
+
+
+for i in range(len(model_list)):
+    f = open(f'air{i}.sh', 'w')
+    f.write(f'#!/bin/bash\n')
+    f.write(f'conda run -n ViT python ../codes/compare_model.py --model {model_list[i]} \n')
+    f.close()
+    os.system(f'sbatch -n1 --gres=gpu:1 -o compare{i}.stdout ./air{i}.sh')
+    os.system(f'rm air{i}.sh')
